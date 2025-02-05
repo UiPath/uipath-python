f2462e8d552a262b7d5a3752ae57586680dff82e
Robert Andrei Moldoveanu <robert.moldoveanu@uipath.com>
2025-02-05 09:27:28 +0200

    feat(cli): add rough cli

---
+++ src/uipath_cli/dummy-main.py
@@ -0,0 +1,7 @@
+import dummy_sdk as sdk
+
+sdk.AssetManager.setAsset("asset1", "value1")
+sdk.AssetManager.setAsset("asset2", 42)
+sdk.AssetManager.setAsset("asset3", True)
+var = "name"
+sdk.AssetManager.setAsset(f"composed {var}", True)
