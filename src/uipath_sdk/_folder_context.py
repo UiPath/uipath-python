fdf1c155be2986b1168308046f53e2fc895d3a72
Gheorghita Hurmuz <gheorghita.hurmuz@uipath.com>
2025-02-10 18:07:23 +0200

    feat: add folder context

---
+++ src/uipath_sdk/_folder_context.py
@@ -0,0 +1,27 @@
+from os import environ as env
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class FolderContext:
+    def __init__(self) -> None:
+        try:
+            self._folder_key: str | None = env["UIPATH_FOLDER_KEY"]
+        except KeyError:
+            self._folder_key = None
+
+        try:
+            self._folder_path: str | None = env["UIPATH_FOLDER_PATH"]
+        except KeyError:
+            self._folder_path = None
+
+    @property
+    def folder_headers(self) -> dict[str, str]:
+        if self._folder_key is not None:
+            return {"x-uipath-folderkey": self._folder_key}
+        elif self._folder_path is not None:
+            return {"X-uipath-folderpath": self._folder_path}
+        else:
+            raise ValueError("Folder key or path is not set (UIPATH_FOLDER_KEY or UIPATH_FOLDER_PATH)")
--- src/uipath_sdk/_uipath_sdk.py
+++ src/uipath_sdk/_uipath_sdk.py
@@ -26,16 +26,13 @@ class UiPathSDK:
             or env.get("UNATTENDED_USER_ACCESS_TOKEN")
             or env.get("UIPATH_ACCESS_TOKEN")
         )
-        folder_id_value = env.get("UIPATH_FOLDER_ID")
 
         self._config = Config(
             base_url=base_url_value,  # type: ignore
             secret=secret_value,  # type: ignore
-            folder_id=folder_id_value,
-            debug=debug,
         )
 
-        setup_logging(self._config.debug)
+        setup_logging(debug)
         log = getLogger("uipath")
 
         log.debug("CONFIG:")
