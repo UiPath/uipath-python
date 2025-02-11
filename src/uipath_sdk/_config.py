43bd580e493525b92048123c4574c120b2489c25
Cristian Pufu <cristian.pufu@uipath.com>
2025-02-11 18:33:59 +0200

    sdk: remove url validation

--- src/uipath_sdk/_config.py
+++ src/uipath_sdk/_config.py
@@ -1,21 +1,6 @@
-from pydantic import BaseModel, HttpUrl, field_validator
-
-PREFIX = "UIPATH_"
-PRODUCT_URL = "uipath.com"
+from pydantic import BaseModel
 
 
 class Config(BaseModel):
     base_url: str
     secret: str
-
-    @field_validator("base_url", mode="before")
-    @classmethod
-    def validate_url(cls, value: str) -> str:
-        # assume that the URL is in the correct format.
-        # https://{domain}.uipath.com/{account_name}/{tenant_name}
-        urlValue = HttpUrl(url=value)
-        assert urlValue.host and urlValue.host.endswith(PRODUCT_URL), "Invalid URL"
-        assert (
-            urlValue.path and len([x for x in urlValue.path.split("/") if x != ""]) == 2
-        ), "Invalid URL"
-        return value
