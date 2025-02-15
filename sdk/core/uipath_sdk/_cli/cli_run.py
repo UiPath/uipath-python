95c430aa4547ef723c941470847302a3eb3b7bab
Cristian Pufu <cristian.pufu@uipath.com>
2025-02-15 12:48:33 +0200

    sdk: fix cli run format

--- sdk/core/uipath_sdk/_cli/cli_run.py
+++ sdk/core/uipath_sdk/_cli/cli_run.py
@@ -7,10 +7,12 @@ from .middlewares import Middlewares
 
 
 @click.command()
-@click.argument('input', required=False, default='{}')
-@click.option('--entrypoint', '-e', help='The entrypoint/graph name')
+@click.argument("input", required=False, default="{}")
+@click.option("--entrypoint", "-e", help="The entrypoint/graph name")
 def run(input: str, entrypoint: Optional[str] = None):
-    should_continue, errorMessage = Middlewares.next("run", input, entrypoint=entrypoint)
+    should_continue, errorMessage = Middlewares.next(
+        "run", input, entrypoint=entrypoint
+    )
 
     if errorMessage:
         click.echo(f"{errorMessage}")
