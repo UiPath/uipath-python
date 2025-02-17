49d1f258ad2d955661846b5afc56c9bdeefe745e
Cristian Pufu <cristian.pufu@uipath.com>
2025-02-17 19:29:01 +0200

    cli: uipath new simple template

--- sdk/core/uipath_sdk/_cli/cli_init.py
+++ sdk/core/uipath_sdk/_cli/cli_init.py
@@ -19,7 +19,7 @@ def generate_env_file(target_directory):
         click.echo(f"Created {relative_path} file.")
         with open(env_path, "w") as f:
             f.write("UIPATH_ACCESS_TOKEN=YOUR_TOKEN_HERE\n")
-            f.write("UIPATH_URL=alpha.uipath.com/ACCOUNT_NAME/TENANT_NAME\n")
+            f.write("UIPATH_URL=https://alpha.uipath.com/ACCOUNT_NAME/TENANT_NAME\n")
 
 
 def get_user_script(directory: str, entrypoint: Optional[str] = None) -> Optional[str]:
