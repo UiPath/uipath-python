aafbc6475d3af7aa8be68033602a063bc32d695f
Robert Andrei Moldoveanu <robert.moldoveanu@uipath.com>
2025-02-25 18:02:46 +0200

    add deploy command

--- sdk/core/uipath_sdk/_cli/__init__.py
+++ sdk/core/uipath_sdk/_cli/__init__.py
@@ -1,5 +1,6 @@
 import click
 
+from .cli_deploy import deploy as deploy  # type: ignore
 from .cli_init import init as init  # type: ignore
 from .cli_new import new as new  # type: ignore
 from .cli_pack import pack as pack  # type: ignore
@@ -17,3 +18,4 @@ cli.add_command(init)
 cli.add_command(pack)
 cli.add_command(publish)
 cli.add_command(run)
+cli.add_command(deploy)
