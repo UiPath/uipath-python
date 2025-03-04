8bc22f4209c967974335a97c2de23cfd9941c976
Gheorghita Hurmuz <gheorghita.hurmuz@uipath.com>
2025-03-04 10:40:40 +0200

    feat(utils): add RequestSpec

--- sdk/core/uipath_sdk/_utils/__init__.py
+++ sdk/core/uipath_sdk/_utils/__init__.py
@@ -1,7 +1,9 @@
 from ._endpoint import Endpoint
 from ._logs import setup_logging
+from ._request_spec import RequestSpec
 
 __all__ = [
     "Endpoint",
     "setup_logging",
+    "RequestSpec",
 ]
