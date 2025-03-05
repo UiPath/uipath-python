1cf8d31dcbc08fefd2d037c227ea214e66e12499
Gheorghita Hurmuz <gheorghita.hurmuz@uipath.com>
2025-03-05 17:18:27 +0200

    feat(sdk): centralize constants

--- sdk/core/uipath_sdk/_execution_context.py
+++ sdk/core/uipath_sdk/_execution_context.py
@@ -3,18 +3,20 @@ from typing import Optional
 
 from dotenv import load_dotenv
 
+from ._utils.constants import ENV_JOB_KEY, ENV_ROBOT_KEY
+
 load_dotenv()
 
 
 class ExecutionContext:
     def __init__(self) -> None:
         try:
-            self._instance_id: Optional[str] = env["UIPATH_JOB_KEY"]
+            self._instance_id: Optional[str] = env[ENV_JOB_KEY]
         except KeyError:
             self._instance_id = None
 
         try:
-            self._robot_key: Optional[str] = env["UIPATH_ROBOT_KEY"]
+            self._robot_key: Optional[str] = env[ENV_ROBOT_KEY]
         except KeyError:
             self._robot_key = None
 
@@ -23,13 +25,13 @@ class ExecutionContext:
     @property
     def instance_id(self) -> Optional[str]:
         if self._instance_id is None:
-            raise ValueError("Instance ID is not set (UIPATH_JOB_KEY)")
+            raise ValueError(f"Instance ID is not set ({ENV_JOB_KEY})")
 
         return self._instance_id
 
     @property
     def robot_key(self) -> Optional[str]:
         if self._robot_key is None:
-            raise ValueError("Robot key is not set (UIPATH_ROBOT_KEY)")
+            raise ValueError(f"Robot key is not set ({ENV_ROBOT_KEY})")
 
         return self._robot_key
