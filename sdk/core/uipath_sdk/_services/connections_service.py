a676497eac35a2a41e427f446240b9cbb708c0ac
Gheorghita Hurmuz <gheorghita.hurmuz@uipath.com>
2025-03-05 09:49:32 +0200

    feat(sdk): add connection service connector protocol and call method

--- sdk/core/uipath_sdk/_services/connections_service.py
+++ sdk/core/uipath_sdk/_services/connections_service.py
@@ -1,3 +1,5 @@
+from typing import Any, Protocol, TypeVar, Union
+
 from uipath_sdk._utils._endpoint import Endpoint
 
 from .._config import Config
@@ -5,15 +7,32 @@ from .._execution_context import ExecutionContext
 from .._models.connections import ConnectionPing
 from ._base_service import BaseService
 
+T_co = TypeVar("T_co", covariant=True)
+
+
+class Connector(Protocol[T_co]):
+    """
+    A protocol for connectors.
+    This helps with type inference when instantiating the connector.
+    Even if we have here a callable, the actual connector should be a class.
+    """
+
+    def __call__(self, *, client: Any, instance_id: Union[str, int]) -> T_co: ...
+
 
 class ConnectionsService(BaseService):
     def __init__(self, config: Config, execution_context: ExecutionContext) -> None:
         super().__init__(config=config, execution_context=execution_context)
 
-    def token(self, elementInstanceId: int) -> ConnectionPing:
+    def __call__(
+        self, connector: Connector[T_co], instance_id: Union[str, int]
+    ) -> T_co:
+        return connector(client=self.client, instance_id=instance_id)
+
+    def token(self, instance_id: int) -> ConnectionPing:
         response = self.request(
             "GET",
-            Endpoint(f"/elements_/v3/element/instances/{elementInstanceId}/ping"),
+            Endpoint(f"/elements_/v3/element/instances/{instance_id}/ping"),
             params={"forcePing": True, "disableOnFailure": True},
         )
 
