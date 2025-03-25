e6a0b07f459db507b68f96f9d92e630fcd4495f9
ionmincu <ion.mincu@uipath.com>
2025-03-25 16:45:29 +0200

    Samples: add local mcp sample

---
+++ samples/travel-planner-local-mcp/main.py
@@ -0,0 +1,95 @@
+import asyncio
+import os
+import sys
+from contextlib import asynccontextmanager
+
+from fix_logger import patch_logger
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.graph import END, START, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode
+from pydantic import BaseModel
+from uipath_langchain.chat.models import (
+    UiPathNormalizedChatModel,
+)
+
+# fix for mcp server
+patch_logger()
+
+
+class GraphInput(BaseModel):
+    message: str
+
+
+class GraphOutput(BaseModel):
+    summary: str
+
+
+def get_file_path(filename):
+    """Get absolute path to file in current directory."""
+    return os.path.abspath(os.path.join(os.getcwd(), filename))
+
+
+def print_environment_variables():
+    """Print all environment variables and their values."""
+    print("Environment Variables:")
+    for key, value in os.environ.items():
+        print(f"{key}: {value}")
+
+
+mcp_config = {
+    "travel_mcp": {
+        "command": "python",
+        "args": [get_file_path("./mcps/travel_mcp.py")],
+        "transport": "stdio",
+    }
+}
+
+
+@asynccontextmanager
+async def make_graph():
+    async with MultiServerMCPClient(mcp_config) as client:
+        tools = client.get_tools()
+        tool_node = ToolNode(tools)
+
+        def should_continue(state: MessagesState):
+            messages = state["messages"]
+            last_message = messages[-1]
+            if last_message.tool_calls:
+                return "tools"
+            return END
+
+        def init(state: GraphInput):
+            print_environment_variables()
+
+            return {
+                "messages": [
+                    SystemMessage(content="You are a helpful assistant!"),
+                    HumanMessage(content=state.message),
+                ]
+            }
+
+        async def call_model(state: MessagesState):
+            messages = state["messages"]
+
+            agent = UiPathNormalizedChatModel().bind_tools(tools)
+            response = agent.invoke(messages)
+
+            return {"messages": [response]}
+
+        workflow = StateGraph(MessagesState, input=GraphInput, output=GraphOutput)
+        # workflow = StateGraph(MessagesState)
+
+        # Define the two nodes we will cycle between
+        workflow.add_node("init", init)
+        workflow.add_node("agent", call_model)
+        workflow.add_node("tools", tool_node)
+
+        workflow.add_edge(START, "init")
+        workflow.add_edge("init", "agent")
+        workflow.add_conditional_edges("agent", should_continue, ["tools", END])
+        workflow.add_edge("tools", "agent")
+
+        graph = workflow.compile()
+
+        yield graph
