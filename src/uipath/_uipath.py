4e110b43dff13a55bf22ccb5bb992aa303840e7e
Alexandru Jircan <alexandru.jircan@uipath.com>
2025-08-27 16:35:24 +0300

    feat: add support for IXP extract and validate

--- src/uipath/_uipath.py
+++ src/uipath/_uipath.py
@@ -13,6 +13,7 @@ from ._services import (
     BucketsService,
     ConnectionsService,
     ContextGroundingService,
+    DocumentsService,
     EntitiesService,
     FolderService,
     JobsService,
@@ -113,6 +114,10 @@ class UiPath:
             self._folders_service,
             self._buckets_service,
         )
+
+    @property
+    def documents(self) -> DocumentsService:
+        return DocumentsService(self._config, self._execution_context)
 
     @property
     def queues(self) -> QueuesService:
