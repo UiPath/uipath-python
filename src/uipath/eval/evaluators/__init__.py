e05bd988e8be9b73d280754783c51e3a2e4fb873
Mayank Jha <mayank.jha@uipath.com>
2025-10-15 07:36:56 +0530

    feat: wiring LLM judge evaluators to new schema

--- src/uipath/eval/evaluators/__init__.py
+++ src/uipath/eval/evaluators/__init__.py
@@ -3,13 +3,13 @@
 from .base_evaluator import LegacyBaseEvaluator
 from .exact_match_evaluator import LegacyExactMatchEvaluator
 from .json_similarity_evaluator import LegacyJsonSimilarityEvaluator
-from .llm_as_judge_evaluator import LlmAsAJudgeEvaluator
+from .llm_as_judge_evaluator import LegacyLlmAsAJudgeEvaluator
 from .trajectory_evaluator import TrajectoryEvaluator
 
 __all__ = [
     "LegacyBaseEvaluator",
     "LegacyExactMatchEvaluator",
     "LegacyJsonSimilarityEvaluator",
-    "LlmAsAJudgeEvaluator",
+    "LegacyLlmAsAJudgeEvaluator",
     "TrajectoryEvaluator",
 ]
