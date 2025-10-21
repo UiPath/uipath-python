a3fe9c9f304d68592f49a1821029fe4e89606e8e
Ion Mincu <ion.mincu@uipath.com>
2025-10-21 15:10:50 +0300

    feat(llm): update endpoints manager

---
+++ src/uipath/telemetry/_constants.py
@@ -0,0 +1,19 @@
+_CONNECTION_STRING = "$CONNECTION_STRING"
+
+_APP_INSIGHTS_EVENT_MARKER_ATTRIBUTE = "APPLICATION_INSIGHTS_EVENT_MARKER_ATTRIBUTE"
+_OTEL_RESOURCE_ATTRIBUTES = "OTEL_RESOURCE_ATTRIBUTES"
+_SDK_VERSION = "SdkVersion"
+_PROJECT_KEY = "ProjectKey"
+
+_TELEMETRY_CONFIG_FILE = ".telemetry.json"
+
+_CODE_FILEPATH = "code.file.path"
+_CODE_FUNCTION = "code.function.name"
+_CODE_LINENO = "code.line.number"
+
+_CLOUD_ORG_ID = "CloudOrganizationId"
+_CLOUD_TENANT_ID = "CloudTenantId"
+_CLOUD_URL = "CloudUrl"
+_APP_NAME = "ApplicationName"
+
+_UNKNOWN = ""
