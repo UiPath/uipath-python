406720112ec81400d814c38e366421313bfa501c
Cristian Pufu <cristian.pufu@uipath.com>
2025-10-24 14:07:19 +0300

    fix: local debug with breakpoints

--- src/uipath/_cli/_runtime/_contracts.py
+++ src/uipath/_cli/_runtime/_contracts.py
@@ -382,6 +382,7 @@ class UiPathRuntimeContext(BaseModel):
     log_handler: Optional[logging.Handler] = None
     chat_handler: Optional[UiPathConversationHandler] = None
     is_conversational: Optional[bool] = None
+    breakpoints: Optional[List[str]] = None
 
     model_config = {"arbitrary_types_allowed": True, "extra": "allow"}
 
