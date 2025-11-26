f683bef49789a9a5f8d2f99400ec053c1474a4c1
Cristian Pufu <cristian.pufu@uipath.com>
2025-11-26 13:46:45 +0200

    fix: update new command

--- src/uipath/_cli/cli_new.py
+++ src/uipath/_cli/cli_new.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 
@@ -26,13 +27,21 @@ version = "0.0.1"
 description = "{project_name}"
 authors = [{{ name = "John Doe", email = "john.doe@myemail.com" }}]
 dependencies = [
-    "uipath>=2.1.174"
+    "uipath>=2.2.0, <2.3.0"
 ]
 requires-python = ">=3.11"
 """
 
     with open(project_toml_path, "w") as f:
         f.write(toml_content)
+
+
+def generate_uipath_json(target_directory):
+    uipath_json_path = os.path.join(target_directory, "uipath.json")
+    uipath_config = {"functions": {"main": "main.py:main"}}
+
+    with open(uipath_json_path, "w") as f:
+        json.dump(uipath_config, f, indent=2)
 
 
 @click.command()
@@ -64,8 +73,10 @@ def new(name: str):
         console.success("Created 'main.py' file.")
         generate_pyproject(directory, name)
         console.success("Created 'pyproject.toml' file.")
+        generate_uipath_json(directory)
+        console.success("Created 'uipath.json' file.")
         init_command = """uipath init"""
-        run_command = """uipath run main.py '{"message": "Hello World!"}'"""
+        run_command = """uipath run main '{"message": "Hello World!"}'"""
         console.hint(f""" Initialize project: {click.style(init_command, fg="cyan")}""")
         console.hint(f"""Run project: {click.style(run_command, fg="cyan")}""")
 
