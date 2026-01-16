e63cb480d75b2989d794eed48d10488946f4f99a
cristian.groza <cristian.groza@uipath.com>
2026-01-16 15:08:12 +0200

    fix: lint issue

--- src/uipath/platform/attachments/attachments.py
+++ src/uipath/platform/attachments/attachments.py
@@ -21,10 +21,7 @@ class Attachment(BaseModel):
     id: Optional[uuid.UUID] = Field(None, alias="ID")
     full_name: str = Field(..., alias="FullName")
     mime_type: str = Field(..., alias="MimeType")
-    model_config = {
-        "title": "UiPathAttachment",
-        "populate_by_name": True
-    }
+    model_config = {"title": "UiPathAttachment", "populate_by_name": True}
 
 
 @dataclass
